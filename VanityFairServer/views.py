f00f7ad04c07ba32d1cc1112fe916a52cc88106b
unHealthy <chenzhutian1992@gmail.com>
2016-04-01 22:42:14 +0800

    add package

--- VanityFairServer/views.py
+++ VanityFairServer/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from VanityFair_server import app
+from VanityFairServer import app
 
 @app.route('/')
 @app.route('/home')
