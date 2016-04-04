6b91f0c1640669f41e1620aa06944e9fd0084b78
unHealthy <chenzhutian1992@gmail.com>
2016-04-05 02:45:07 +0800

    async crawler with repeatation when failed to access

--- DataCenter/Crawler.py
+++ DataCenter/Crawler.py
@@ -1,100 +1,30 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
 from bs4 import BeautifulSoup as bs
 import requests
+import gevent
 from pymongo import MongoClient
 
-def THSDetailDataUrlGen(stockCode,date):
-    return "http://data.10jqka.com.cn/market/lhbcjmx/code/"+stockCode+"/date/"+date+"/ajax/1"
-
-def GetBillBoardDetailDataFromTHS(stockCode, date):
-    pass
-
-def THSOverviewDataUrlGen(date,page = 1):
-    return "http://data.10jqka.com.cn/market/longhu/date/"+date+"/ajax/1/page/"+ str(page)
-
-def GetBillBoardDataFromTHSOn(date = None):
-    # connect to mongodb
-    client = MongoClient()
-    db = client.BillBoard
-    collection = db.BillBoardEveryDay
-
-    date = "2015-11-15"
-    #date = "2016-04-01"
-    r = requests.get(THSOverviewDataUrlGen(date))
-
-    heads = []
-
-    if(r.status_code):
-        #print(r.text)
-        soup = bs(r.text,"lxml")
-        
-        # get page count
-        pageCount = soup.find(id="lhdata").find(id="m_page")
-        if pageCount: 
-            pageCount = int(pageCount.find("span",class_="page_info").text.split('/')[-1])
-        else:
-            pageCount = 1
-        print(pageCount)
-
-        # get table data
-        table = soup.find(id="lhdata").find(id="maintable")
-        # get table heads
-        tHead = table.find("thead")
-        headRow = tHead.find("tr")
-        headNames = headRow.find_all("th")
-        heads = [ele.text.strip() for ele in headNames]
-        # get table body
-        tBody = table.find("tbody")
-        for row in tBody.find_all("tr"):
-            cols = row.find_all('td')
-            if(len(cols) == 1): break
-            doc = {};
-            for idx,val in enumerate(cols):
-                if(heads[idx] == "序号" or heads[idx] == "相关链接"):continue
-                doc[heads[idx]] = val.text.strip()
-            doc["日期"] = date
-            collection.insert_one(doc)
-        
-        currentPage = 2
-        while currentPage <= pageCount:
-            r = requests.get(THSOverviewDataUrlGen(date,currentPage))
-            if(r.status_code):
-                soup = bs(r.text,"lxml")
-                # get table data
-                table = soup.find(id="lhdata").find(id="maintable")
-                # get table body
-                tBody = table.find("tbody")
-                for row in tBody.find_all("tr"):
-                    cols = row.find_all('td')
-                    doc = {};
-                    for idx,val in enumerate(cols):
-                        if(heads[idx] == "序号" or heads[idx] == "相关链接"):continue
-                        doc[heads[idx]] = val.text.strip()
-                    doc["日期"] = date
-                    collection.insert_one(doc)
-            currentPage += 1
-            
+requestHeaders = {'user-agent':"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
+# connect to mongodb
+client = MongoClient()
+db = client.BillBoard
+collection = db.BillBoardEveryDay
 
 def EMOverviewDataUrlGen(date):
-    return "http://data.eastmoney.com/stock/lhb/"+date+".html"
+    return "http://data.eastmoney.com/stock/lhb/" + date + ".html"
 
 def GetBillBoardDataFromEM(date):
-    # connect to mongodb
-    client = MongoClient()
-    db = client.BillBoard
-    collection = db.BillBoardEveryDay
+    r = requests.get(EMOverviewDataUrlGen(date), headers = requestHeaders)
+    while(r.status_code != requests.codes.ok):
+        r = requests.get(EMOverviewDataUrlGen(date), headers = requestHeaders)
 
-    date = "2015-11-01"
-    #date = "2016-04-01"
-    r = requests.get(EMOverviewDataUrlGen(date))
-
-    heads = []
-
-    if(r.status_code):
+    if(r.status_code == requests.codes.ok):
         #print(r.text)
         soup = bs(r.text,"lxml")
-        
         # get table data
-        table = soup.find(id="dt_1",recursive=False)
+        table = soup.find(id="dt_1")
         # get table heads
         tHead = table.find("thead")
         headRow = tHead.find("tr")
@@ -104,69 +34,98 @@ def GetBillBoardDataFromEM(date):
         tBody = table.find("tbody")
         rowSpanData = []
         rowSpanCount = 0
-        for row in tBody.find_all("tr"):
+        docs = {}
+        stockCodes = set()
+        for row in tBody.find_all("tr",class_="all"):
             cols = row.find_all('td')
             if(len(cols) == 1): break
-            doc = {};
+            doc = {}
+            shouldGetDetail = False
 
+            ## whether it is rowspan
             if(rowSpanCount > 0):
                 for idx,val in enumerate(rowSpanData):
-                    if(heads[idx] == "序号" or heads[idx] == "相关链接"):continue
+                    if(heads[idx] == "序号" or heads[idx] == "相关链接"):
+                        continue
                     doc[heads[idx]] = val
             else:
                 rowSpanData = []
                 rowSpanCount = 0
+                shouldGetDetail = True
 
+            ## get data from each columns
             idxOffset = len(rowSpanData)
             for idx,val in enumerate(cols):
                 # set row Span
                 if val.has_attr("rowspan"): 
                     rowSpanCount = int(val["rowspan"])
                     rowSpanData.append("".join(val.text.split()))
-                if(heads[idx+idxOffset] == "序号" or heads[idx+idxOffset] == "相关链接"):continue
-
-                doc[heads[idx+idxOffset]] = "".join(val.text.split())
-
+                if(heads[idx + idxOffset] == "序号" or 
+                   heads[idx + idxOffset] == "相关链接" or
+                   heads[idx + idxOffset] == "占总成交比例"):
+                    continue
+                doc[heads[idx + idxOffset]] = "".join(val.text.split())
             doc["日期"] = date
-            collection.insert_one(doc)
-            rowSpanCount -= 1
+            stockCodes.add(doc["代码"])
+            if doc["代码"] not in docs: 
+                docs[doc["代码"]] = []
+            docs[doc["代码"]].append(doc)
+
+            if(rowSpanCount > 0): 
+                rowSpanCount -= 1
+
+        threads = [gevent.spawn(AsyncMergeBillBoardDetailDataToOverviewDataFromEM,docs[stockCode],date,stockCode) for stockCode in stockCodes]
+        gevent.joinall(threads)
+
     else:
         print(r.status_code)
-      
+ 
+def AsyncMergeBillBoardDetailDataToOverviewDataFromEM(docs,date,stockCode):
+    print("get"+stockCode)
+    detailDocs = GetBillBoardDetailDataFromEM(date,stockCode)
+    print("get finish")
+    for i in range(len(detailDocs)):
+        detailDoc = detailDocs[i]
+        targetDoc = None
+        for doc in docs:
+            if detailDoc["上榜原因"] == doc["上榜原因"]:
+                targetDoc = doc
+        for key in detailDoc:
+            targetDoc[key] = detailDoc[key]
+        collection.insert_one(targetDoc)
+             
 def EMDetailviewDataUrlGen(date,stockCode):
-    return "http://data.eastmoney.com/stock/lhb,"+date+","+stockCode+".html"
+    return "http://data.eastmoney.com/stock/lhb," + date + "," + stockCode + ".html"
 
 def GetBillBoardDetailDataFromEM(date,stockCode):
-    # connect to mongodb
-    client = MongoClient()
-    db = client.BillBoard
-    collection = db.BillBoardDetail
-
-    r = requests.get(EMDetailviewDataUrlGen(date,stockCode))
-    heads = []
-
-    if(r.status_code):
-        #print(r.text)
+    r = requests.get(EMDetailviewDataUrlGen(date,stockCode),headers = requestHeaders)
+    while r.status_code != requests.codes.ok:
+        r = requests.get(EMDetailviewDataUrlGen(date,stockCode),headers = requestHeaders)
+    docs = []
+    if(r.status_code == requests.codes.ok):
         soup = bs(r.text,"lxml")
         
         # get table data
-        tables = soup.find(id="cont1").find_all("table",recursive=False)
-        divtips = soup.find(id="cont1").find_all("div",class_="divtips")
+        targetDiv = soup.find(id="cont1")
+        tables = targetDiv.find_all("table")
+        divtips = targetDiv.find_all("div",class_="divtips")
         for i  in range(len(divtips)):
             doc = {}
-
+            ## get tips data
             tips = divtips[i].find_all("li")
             tip1 = tips[0].text.replace("：",":").split(":")
-            doc[tip1[0]] = tip1[1] 
+            doc["上榜原因" if tip1[0] == "类型" else tip1[0]] = tip1[1] 
 
             for item in tips[1].text.replace("：",":").split():
-                doc[item.split(":")[0]] = item.split(":")[1]
+                item = item.split(":")
+                doc[item[0]] = item[1]
   
-            table = tables[i*2]
+            ## get table head
+            table = tables[i * 2]
             headRow = table.find("thead").find_all("tr")[1]
             headNames = headRow.find_all("th")
             heads = ["".join(ele.text.split()) for ele in headNames]
-            ## get table body
+            ## get buyer table body
             tBody = table.find("tbody")
             buyers = {}
             for index,row in enumerate(tBody.find_all("tr")):
@@ -178,10 +137,11 @@ def GetBillBoardDetailDataFromEM(date,stockCode):
                     # set row Span
                     if(heads[idx] == "序号"):continue
                     oneBuyer[heads[idx]] = "".join(val.text.split())
-                buyers["b"+str(index)] = oneBuyer
+                buyers["b" + str(index)] = oneBuyer
             doc["buyers"] = buyers
 
-            tBody = tables[i*2 + 1].find("tbody")
+            ## get seller table body
+            tBody = tables[i * 2 + 1].find("tbody")
             sellers = {}
             rows = tBody.find_all("tr")
             rowsCount = len(rows)
@@ -195,12 +155,17 @@ def GetBillBoardDetailDataFromEM(date,stockCode):
                     # set row Span
                     if(heads[idx] == "序号"):continue
                     oneSeller[heads[idx]] = "".join(val.text.split())
-                sellers["s"+str(index)] = oneSeller
+                sellers["s" + str(index)] = oneSeller
             doc["sellers"] = sellers
-            for idx,val in enumerate(rows[rowsCount - 1].find_all('td')):
-                if(idx == 0):continue
-                doc[heads[idx+1]] = "".join(val.text.split())
-       
-            collection.insert_one(doc)
+
+            ## get statistics data from last row
+            #for idx,val in enumerate(rows[rowsCount - 1].find_all('td')):
+            #    if(idx == 0):continue
+            #    doc[heads[idx + 1]] = "".join(val.text.split())
+            doc['日期'] = date
+            doc['代码'] = stockCode
+            docs.append(doc)
     else:
         print(r.status_code)
+    
+    return docs
--- runserver.py
+++ runserver.py
@@ -5,11 +5,11 @@ This script runs the VanityFair_server application using a development server.
 from os import environ
 from DataCenter import BillBoard as bb
 from DataCenter import Crawler as cr
-from VanityFairServer import app
 
 if __name__ == '__main__':
     print("dd")
-    cr.GetBillBoardDetailDataFromEM("2015-11-10","002289")
+    cr.GetBillBoardDataFromEM("2015-11-10")
+    #print(cr.GetBillBoardDetailDataFromEM("2015-11-10","300359"))
     #HOST = environ.get('SERVER_HOST', 'localhost')
     #try:
     #    PORT = int(environ.get('SERVER_PORT', '5555'))
